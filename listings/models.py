269803a42a43c55142e9bba406b9ea9241f4ba2e
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-10 19:45:40 +0530

    Set up the admin area

--- listings/models.py
+++ listings/models.py
@@ -26,5 +26,6 @@ class Listing(models.Model):
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
 
- def __str__(self):
+def __str__(self):
     return self.title
+    
