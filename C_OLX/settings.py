4a574d714203d8023b0be6ba852643588be6de2f
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-13 19:54:39 +0530

    Pulled data to frontend and added pagination

--- C_OLX/settings.py
+++ C_OLX/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'django.contrib.humanize',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Listing
 
+
+
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published',
                   'price', 'list_date', 'realtor')
