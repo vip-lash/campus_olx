09c28239e8717bad750bf26fea37f7552740349a
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-21 00:12:08 +0530

    Change in settings.py

--- C_OLX/settings.py
+++ C_OLX/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'C_OLX/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'C_OLX/static'),
 )
