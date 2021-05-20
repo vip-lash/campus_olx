574aa23dc6f8660064f099e4d0a154ec7b9dcc39
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-20 23:10:19 +0530

    Set up the environment variables

--- C_OLX/settings.py
+++ C_OLX/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7rxqy6e$+#h3#w0j=#wx_7&g4v5+1(a)yoq*9k3n-lr54)tq+v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['campusolx.herokuapp.com']
 
 
 # Application definition
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'C_OLX',
-        'USER': 'postgres',
-        'PASSWORD': '8887889112',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': 'localhost',
     }
 }
