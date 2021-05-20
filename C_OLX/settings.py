88d390909a072dff8b6e6183c440e8b0bdc5100c
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-21 00:02:25 +0530

    Change in settings.py

--- C_OLX/settings.py
+++ C_OLX/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'C_OLX/static'),
 )
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
