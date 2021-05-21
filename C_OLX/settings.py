7799c8dfee85b39f72ec66ee3dfb872d8f22dff9
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-21 15:13:24 +0530

    Change in settings.py

--- C_OLX/settings.py
+++ C_OLX/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['campusolx.herokuapp.com']
+ALLOWED_HOSTS = ['campusolx.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
