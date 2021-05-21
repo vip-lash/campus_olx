bba62cf16c2ca12100030f86bafab307374b9a89
vip-lash <79157623+vip-lash@users.noreply.github.com>
2021-05-21 14:59:16 +0530

    Change in settings.py

--- C_OLX/settings.py
+++ C_OLX/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'i$1m&2!nvt7$n16($v=&x=*(5+v1kp=espbdn%0&4zdra5n_$!'
+#SECRET_KEY = ''
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['campusolx.herokuapp.com', '127.0.0.1:8000']
 
